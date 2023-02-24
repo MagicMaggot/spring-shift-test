472750424f5b21bd8912777c971fe1be55e5bb61
Alexander Ikaev <smea9000@gmail.com>
2023-02-24 14:33:35 +0300

    init

---
+++ src/test/java/com/example/springshifttest/SpringShiftTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springshifttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShiftTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
